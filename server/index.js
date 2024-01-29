4fb2f36464e7bccf63a6679a0d0b80690c3624a4
Abhishek Kumar Verma <abhishek27501@gmail.com>
2024-01-29 18:11:02 +0530

    installtion of express and basics

--- server/index.js
+++ server/index.js
@@ -1,32 +1,46 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
+// const http = require('http')
+// const fs = require('fs')
+// const url = require('url')
+const express = require("express")
+const app = express();
 
-const myServer = http.createServer((req, res) => {
+app.get('/', (req, res) => {
+    return res.send("Hello From Home Page")
+})
+app.get('/about', (req, res) => {
+    return res.send("Hello From About Page")
+})
+app.get('/contact', (req, res) => {
+    return res.send("Hello From Contact Page")
+})
 
-    const log = `${Date.now()} : ${req.url} New Req recieved \n`
+app.listen(7000, ()=>{
+    console.log('Server Started !');
+})
 
-    const myUrl = url.parse(req.url,true)
-    console.log(myUrl);
-    fs.appendFile('log.txt', log, (err, data) => {
+// function myHandler( (req, res) => {
+
+//     const log = `${Date.now()} : ${req.url} New Req recieved \n`
+//     const myUrl = url.parse(req.url,true)
+//     console.log(myUrl);
+//     fs.appendFile('log.txt', log, (err, data) => {
         
-        switch(myUrl.pathname){
-            case '/': res.end('Home page')
-            break;
-            case '/about':
-                // const username = myUrl.query.myname
-                res.end('About page')
-            break;
-            case '/contact':res.end('contact page')
-            break;
-            default:
-                res.end('404 Page not found')
-        }
-    })
-    // console.log("New request rec.");
-    // console.log(req);
-});
+//         switch(myUrl.pathname){
+//             case '/': res.end('Home page')
+//             break;
+//             case '/about':
+//                 // const username = myUrl.query.myname
+//                 res.end('About page')
+//             break;
+//             case '/contact':res.end('contact page')
+//             break;
+//             default:
+//                 res.end('404 Page not found')
+//         }
+//     })
+// })
+// const myServer = http.createServer(app);
 
-myServer.listen(7000, () =>{
-    console.log("Server Started !");
-});
+// myServer.listen(7000, () =>{
+//     console.log("Server Started !");
+// });
