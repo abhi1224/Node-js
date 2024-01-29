4830c691374358bb14f79308503b4c3dd375e1a3
Abhishek Kumar Verma <abhishek27501@gmail.com>
2024-01-29 16:28:11 +0530

    blocking and non-blocking element

--- file.js
+++ file.js
@@ -1,5 +1,7 @@
 const fs = require("fs")
+const os = require("os")
 
+console.log(os.cpus().length);
 // // Sync
 // fs.writeFileSync("./text.txt", "Hey There")
 
@@ -8,22 +10,29 @@ const fs = require("fs")
 
 
 // readFileSync - return a value
+
+console.log("1");
+// Blocking ... 
 const result = fs.readFileSync('./contacts.txt', "utf-8")
 console.log(result);
 
+console.log("2");
+
+
 // readFileSync - not return a value
 
+// Non-Blocking
+console.log("1");
+
 fs.readFile('./contacts.txt', "utf-8", (err, result) => {
-    if(err){
-        console.log('Error', err);
-    }
-    else{
-        console.log(result);
-    }
+     console.log(result);   
 })
+console.log("2");
+
 
 
 fs.appendFileSync("./text.txt", "hello")
 
 //  fs.cpSync("./text.txt", "./copy.txt")
-fs.unlinkSync("./copy.txt")
+// fs.unlinkSync("./copy.txt")
+ 
