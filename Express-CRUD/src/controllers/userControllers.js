a3e20928d47d55b12c311010d66205a7ca0aaeaf
Abhishek Kumar Verma <abhishek27501@gmail.com>
2026-01-08 16:35:52 +0530

    func: created the addUserController for adding users to database in express

--- Express-CRUD/src/controllers/userControllers.js
+++ Express-CRUD/src/controllers/userControllers.js
@@ -30,5 +30,31 @@ export const getUserController = async (req, res) => {
 };
 
 export const addUserController = async(req, res) => {
-    console.log('hello from add user controller');    
+    try{
+        const {firstName, lastName, age, city} = req.body
+        if(!firstName || !lastName || !age || !city){
+                return res.status(400).json({
+                        success: false,
+                        statusCode: 400,
+                        message: "All required fields must be provided"
+                    });
+        }
+        const newUser = await User.create(req.body);
+        
+        return res.status(201).json({
+            success: true,
+            statusCode: 201,
+            message: "User data inserted successfully",
+            user: newUser
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            statusCode: 500,
+            message: "Something went wrong while adding user",
+            error: error.message
+        });
+    }
 } 
+
--- Express-CRUD/src/routes/userRoute.js
+++ Express-CRUD/src/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addUserController, getUserController } from "../controllers/userControllers.js";
+import { addUserController, getUserByIdController, getUserController } from "../controllers/userControllers.js";
 
 const router = express.Router()
 
 router.get('/', getUserController)
 router.post('/', addUserController)
+router.get('/:id', getUserByIdController)
 
 export default router;
